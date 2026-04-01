fdc270080b824f78233ac6b1fe5410fa3a026394
ThakurKrishna45 <215252541+ThakurKrishna45@users.noreply.github.com>
2026-04-01 10:51:37 +0530

    Intial commit

---
+++ src/main/java/com/capgemini/taskmanagementsystem_frontend/TaskmanagementsystemFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.taskmanagementsystem_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagementsystemFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagementsystemFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capgemini/taskmanagementsystem_frontend/TaskmanagementsystemFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.taskmanagementsystem_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagementsystemFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
