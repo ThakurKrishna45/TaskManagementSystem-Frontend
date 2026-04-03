9db8af16bc287a443a2c6080397b4ceb3f7644c2
adityaKumar0909 <kadi93030@gmail.com>
2026-04-03 19:09:26 +0530

    changed NotificationResponseDTO and added some CSS

--- src/main/java/com/capgemini/taskmanagementsystem_frontend/entity/NotificationDto.java
+++ src/main/java/com/capgemini/taskmanagementsystem_frontend/entity/NotificationDto.java
@@ -3,10 +3,18 @@ package com.capgemini.taskmanagementsystem_frontend.entity;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Data
 public class NotificationDto {
     private Integer notificationID;
     private String text;
     private LocalDateTime createdAt;
+    private String fullName;
+
+    public String getFormattedDate() {
+        if (createdAt == null) return "";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm a, dd MMM yyyy");
+        return createdAt.format(formatter);
+    }
 }
